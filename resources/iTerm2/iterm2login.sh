dc7f58c7ea43350a00e3a67ce192c2bedb28e6ab
Charies Gavin <guobinhit@qq.com>
2017-11-08 17:55:19 +0800

    add resources package

---
+++ resources/iTerm2/iterm2login.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/expect
+
+set timeout 30
+spawn ssh -p [lindex $argv 0] [lindex $argv 1]@[lindex $argv 2]
+expect {
+        "(yes/no)?"
+        {send "yes\n";exp_continue}
+        "password:"
+        {send "[lindex $argv 3]\n"}
+}
+interact
+
+# Using in item2 save account and password, add by bin.guo 20171107
+
+
